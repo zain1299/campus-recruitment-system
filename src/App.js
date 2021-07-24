0fc597c04ed1c5b86a3a4ee88d952bfbfeb28258
zain1299 <zain.kiyani1999@gmail.com>
2021-07-24 15:53:04 +0500

    campus-recruitment-system on React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Signin from "./containners/Signin/Signin";
+import SignUp from "./containners/SignUp/SignUp";
+
+function App() {
+  return (
+    <div className="App">
+      <Signin />
+      {/* <SignUp /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containners/Signin/Signin.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button, Form, Image } from "react-bootstrap";
+import BtnPrimary from "../../Components/Button";
+import TextInput from "../../Components/TextInput";
+import logo from "../../Assets/Images/logo.png";
+import "../../Styles/Containners/Signin.css";
+
+function Signin() {
+  return (
+    <div className="SigninMain">
+      <form className="SigninForm">
+        <Image className="compLogo" src={logo} alt="logo" roundedCircle />
+
+        <TextInput placeHolder="Email" id="email" type="email" name="email" />
+
+        <TextInput
+          placeHolder="Password"
+          type="password"
+          id="password"
+          name="password"
+        />
+        <Form.Group>
+          <Form.Check
+            style={{
+              display: "flex",
+              marginTop: "0.199rem",
+              marginBottom: "1rem",
+            }}
+            type="checkbox"
+            label="Remember me "
+          />
+        </Form.Group>
+        <BtnPrimary type="submit" title="Signin" />
+        <Button variant="link" type="button">
+          Don't have an account ?
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default Signin;
